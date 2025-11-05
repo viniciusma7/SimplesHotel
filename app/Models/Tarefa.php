1b78d77dc67c2c0b5619e1747af8665b30cc0c72
Vinicius <vinicius.almeida@petroeng.com.br>
2025-11-05 12:01:03 -0300

    Criando model e migration para tarefas

---
+++ app/Models/Tarefa.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tarefa extends Model
+{
+    protected $fillable = [
+        'titulo',
+        'descricao',
+        'concluida',
+    ];
+}
